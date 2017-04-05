6ed36e180a1ee112430b1eca9d5ce044bacd4e22
Gordon McCreight <gordon@mccreight.com>
2017-04-05 14:11:06 -0700

    Allow passing url to exporter

--- from-kinesis/druid_exporter_tester.rb
+++ from-kinesis/druid_exporter_tester.rb
@@ -15,11 +15,13 @@ require_relative 'druid_exporter'
 shard_id = "shardId-000000000000"
 
 debug_level = 3 # Show absolutely everything
-exporter = DruidExporter.new(shard_id, chunk_size: 3, debug_level: debug_level)
+local_exporter_url = "http://localhost:8200/v1/post/eventer"
 
-exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 17:24:53 UTC",":event_type":":info",":uuid":"ed7ee1ad-aa4d-4cda-af10-a2c00dac60f2"}~)
-exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 17:25:15 UTC",":event_type":":info",":uuid":"b0c6170e-41ea-4169-942b-9e4757570543",":item_id":2}~)
-exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 17:25:17 UTC",":event_type":":info",":uuid":"78c617cc-41ea-4169-942b-9e4757570999",":item_id":4}~)
-exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 17:25:17 UTC",":event_type":":info",":uuid":"78c617cc-41ea-4169-942b-9e4757570999",":item_id":4}~)
+exporter = DruidExporter.new(shard_id, chunk_size: 3, exporter_url: local_exporter_url, debug_level: debug_level)
+
+exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 20:34:53 UTC",":event_type":":info",":uuid":"ed7ee1ad-aa4d-4cda-af10-a2c00dac60f2"}~)
+exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 20:35:15 UTC",":event_type":":info",":uuid":"b0c6170e-41ea-4169-942b-9e4757570543",":item_id":2}~)
+exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 20:35:17 UTC",":event_type":":info",":uuid":"78c617cc-41ea-4169-942b-9e4757570999",":item_id":4}~)
+exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 20:35:17 UTC",":event_type":":info",":uuid":"78c617cc-41ea-4169-942b-9e4757570999",":item_id":4}~)
 
 exporter.finish
