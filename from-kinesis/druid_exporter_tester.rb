2a187b61018814058bcfe593a217239380289948
Gordon McCreight <gordon@mccreight.com>
2017-04-05 15:31:20 -0700

    Make the export tester create event_date_time that always work

--- from-kinesis/druid_exporter_tester.rb
+++ from-kinesis/druid_exporter_tester.rb
@@ -19,9 +19,9 @@ local_exporter_url = "http://localhost:8200/v1/post/eventer"
 
 exporter = DruidExporter.new(shard_id, chunk_size: 3, exporter_url: local_exporter_url, debug_level: debug_level)
 
-exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 20:34:53 UTC",":event_type":":info",":uuid":"ed7ee1ad-aa4d-4cda-af10-a2c00dac60f2"}~)
-exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 20:35:15 UTC",":event_type":":info",":uuid":"b0c6170e-41ea-4169-942b-9e4757570543",":item_id":2}~)
-exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 20:35:17 UTC",":event_type":":info",":uuid":"78c617cc-41ea-4169-942b-9e4757570999",":item_id":4}~)
-exporter.add_row(%q~{":event_name":"foo",":event_date_time_utc":"2017-04-05 20:35:17 UTC",":event_type":":info",":uuid":"78c617cc-41ea-4169-942b-9e4757570999",":item_id":4}~)
+exporter.add_row(%Q~{":event_name":"foo",":event_date_time_utc":"#{Time.now - 40}",":event_type":":info",":uuid":"ed7ee1ad-aa4d-4cda-af10-a2c00dac60f2"}~)
+exporter.add_row(%Q~{":event_name":"foo",":event_date_time_utc":"#{Time.now - 30}",":event_type":":info",":uuid":"b0c6170e-41ea-4169-942b-9e4757570543",":item_id":2}~)
+exporter.add_row(%Q~{":event_name":"foo",":event_date_time_utc":"#{Time.now - 20}",":event_type":":info",":uuid":"78c617cc-41ea-4169-942b-9e4757570999",":item_id":4}~)
+exporter.add_row(%Q~{":event_name":"foo",":event_date_time_utc":"#{Time.now - 10}",":event_type":":info",":uuid":"55c617cc-41ea-4169-942b-9e4757570999",":item_id":4}~)
 
 exporter.finish
